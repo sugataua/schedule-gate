f58f0428cec8c41565259365934f0bf3d737b1c7
Sugata <sugataua@sugataua.com>
2017-05-12 11:50:08 +0300

    Cleaning code, refactoring handleKeyboard function, adding comments

--- schedule_gate/static/app.js
+++ schedule_gate/static/app.js
@@ -1,5 +1,16 @@
+var ESCAPE_CODE = 27;
+var ENTER_CODE = 13;
+var TAB_CODE = 9;
+var UP_ARROW_CODE = 38;
+var DOWN_ARROW_CODE = 40;
+
+var LOADER_MESSAGE = "Завантаження...";
+
 var search_result;
 
+/*
+	Getting required DOM objects
+ */
 var input_from = document.getElementById("settlement-search-from");
 var input_to = document.getElementById("settlement-search-to");
 var search_form = document.getElementById('form-settlement-search');
@@ -7,204 +18,212 @@ var search_form = document.getElementById('form-settlement-search');
 var from_feedback_icon = document.getElementById('settlement-search-from-feedback-icon');
 var to_feedback_icon = document.getElementById('settlement-search-to-feedback-icon');
 
+/*
+	Setting default state for input field icons
+ */
 from_feedback_icon.style.display = 'none';
 to_feedback_icon.style.display = 'none';
 
+// Get correct XHR
 function getXMLHttpRequest() {
 	var xmlHttp;
 	if (window.XMLHttpRequest) {
 	// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlHttp=new XMLHttpRequest();
+		xmlHttp = new XMLHttpRequest();
 	} else {  // code for IE6, IE5
-		xmlHttp=new ActiveXObject("Microsoft.XMLHTTP");
+		xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
 	}
+
 	return xmlHttp;
 }
 
-
-function addHighlightSearchResult(event){
-	var result_container = this.parentElement;
-	var selected_list = result_container.getElementsByClassName('search-result-selected');
-	for (var i=0; i<selected_list.length; i++) {
-		selected_list[i].classList.remove('search-result-selected');
-	}
-	this.classList.add('search-result-selected');
-
-}
-
-
-function removeHighlightSearchResult(event){
-	this.classList.remove('search-result-selected');
-}
-
+// To display settlement names correctly
 function capitalizeFirstLetter(string) {
 	string = string.toLowerCase();
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+// Add highlight on mouse hover
+function addHighlightSearchResult(event){
+	var result_container = this.parentElement;
+	var selected_list = result_container.getElementsByClassName('search-result-selected');
 
+	for (var i=0; i<selected_list.length; i++) {
+		selected_list[i].classList.remove('search-result-selected');
+	}
+
+	this.classList.add('search-result-selected');
+}
+
+// Remove highlight when mouse out
+function removeHighlightSearchResult(event){
+	this.classList.remove('search-result-selected');
+}
+
+// Select settlement by click
 function selectSettlement() {
 	
-	var livesearch_div = this.parentElement;
-	var input = livesearch_div.parentElement.previousElementSibling; 
-	//var settl_id = document.getElementById("settlement-id");
-	
-	
-	settl = search_results[this._ind];	
-	// console.log('Input:');
-	// console.log(input);
-	input.value = capitalizeFirstLetter(settl.name);
+	var settlement;
+	var suggestions_box = this.parentElement;
+	var input = suggestions_box.parentElement.previousElementSibling;
+	var feedback_icon = suggestions_box.parentElement.nextElementSibling;
+
+	settlement = search_results[this._ind];
+
+	input.value = capitalizeFirstLetter(settlement.name);
 	input._sttl_obj = this._sttl_obj;
-	//settl_id.value = settl.id;
+
+	// Display successful selection on UI
 	input.parentElement.classList.remove("has-error");
 	input.parentElement.classList.add('has-success');
-	
-	
-	livesearch_div.style.display = 'none';
-
-	var feedback_icon = livesearch_div.parentElement.nextElementSibling;
+	suggestions_box.style.display = 'none';
 	feedback_icon.style.display = 'block';
-
 }
 
+// Select settlement by TAB and ENTER
+function selectSettlementBySuggestion(result_div) {
 
-function selectSettlementonEnter(result_div) {
+	var settlement;
+	var suggestion_box = result_div.parentElement;
+	var input = suggestion_box.parentElement.previousElementSibling;
+	var feedback_icon = suggestion_box.parentElement.nextElementSibling;
 
-	var livesearch_div = result_div.parentElement;
-	var input = livesearch_div.parentElement.previousElementSibling;
-	//var settl_id = document.getElementById("settlement-id");
+	settlement = search_results[result_div._ind];
 
-
-	settl = search_results[result_div._ind];
-	// console.log('Input:');
-	// console.log(input);
-	input.value = capitalizeFirstLetter(settl.name);
+	input.value = capitalizeFirstLetter(settlement.name);
 	input._sttl_obj = result_div._sttl_obj;
-	//settl_id.value = settl.id;
+
+	// Display successful selection on UI
 	input.parentElement.classList.remove("has-error");
 	input.parentElement.classList.add('has-success');
-
-
-	livesearch_div.style.display = 'none';
-
-	var feedback_icon = livesearch_div.parentElement.nextElementSibling;
+	suggestion_box.style.display = 'none';
 	feedback_icon.style.display = 'block';
 
 }
 
-
+// Get list of settlements from server and display them into suggestion box
 function showSearchSuggestions() {
-	/* Get list of settlements from server and display them into suggestion box */
+
+	var xmlHttp;
+
+	// Getting current input
 	var input_text = this.value;
+
+	// Clear previous selection
 	this._sttl_obj = null;
 
+	// Hide success icon
 	this.nextElementSibling.nextElementSibling.style.display = 'none';
+
+	// Getting form-group div
 	var livesearch_div_parent = this.parentNode;
 	livesearch_div_parent.classList.remove('has-success');
-	
-	// console.log(livesearch_div_parent);
-	
+
+	// Getting suggestion box div
 	var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
-	// console.log(result_of_livesearch);	
-	var livesearch_div = result_of_livesearch[0]; 
+	var suggestion_box = result_of_livesearch[0];
 	
-	if (input_text.length <= 1) { 
-		livesearch_div.innerHTML="";
-		livesearch_div.style.display = 'none';    
+	// Do not show suggestions if typed text is less than 1 symbol
+	if (input_text.length <= 1) {
+		suggestion_box.innerHTML="";
+		suggestion_box.style.display = 'none';
 		return;
 	}
   
-	livesearch_div.innerHTML = '';
-	livesearch_div.style.display = 'block';
-	livesearch_div.classList.add('livesearch-has-result');
+	// Clear suggestion box
+	suggestion_box.innerHTML = '';
+	suggestion_box.style.display = 'block';
+	suggestion_box.classList.add('livesearch-has-result');
 	
-	var settl_div_f = document.createElement('div');
-	settl_div_f.className = "search-result";
+	// Create div for loader
+	var suggestion_loader = document.createElement('div');
+	suggestion_loader.className = "search-result";
 	
-	var settl_name_f = document.createElement('span');
-	settl_name_f.classList.add('glyphicon');
-	settl_name_f.classList.add('glyphicon-refresh');
-	settl_name_f.classList.add('gly-spin');
-	
-	settl_div_f.appendChild(settl_name_f);
-	settl_div_f.innerHTML = settl_div_f.innerHTML + '<b>Завантаження...</b>';
-	
-	livesearch_div.appendChild(settl_div_f);
-	/*
-	livesearch_div.style.display = 'block';
-	livesearch_div.style.border="1px";
-	*/
-	
-	xmlhttp = getXMLHttpRequest();
+	// Set loader icon
+	var suggestion_loader_icon = document.createElement('span');
+	suggestion_loader_icon.classList.add('glyphicon');
+	suggestion_loader_icon.classList.add('glyphicon-refresh');
+	suggestion_loader_icon.classList.add('gly-spin');
+	suggestion_loader.appendChild(suggestion_loader_icon);
 
-	xmlhttp.onreadystatechange=function() {
-    if (this.readyState==4 && this.status==200) {
-	
-	  livesearch_div.innerHTML = '';
-	  //search_results = document.getElementById("livesearch")
-	  search_results = JSON.parse(this.responseText);
-	  // console.log(search_results[0]);
-	  search_results.innerHTML = "";
-	  for (var i=0; i<search_results.length; i++) {
-		  var settl_div = document.createElement('div');
-		  
-		  settl_div.className = "search-result";
+	// Add loader
+	suggestion_loader.innerHTML = suggestion_loader.innerHTML + '<b>' + LOADER_MESSAGE + '</b>';
+	suggestion_box.appendChild(suggestion_loader);
 
-		  settl_div.onmouseover = addHighlightSearchResult;
-		  settl_div.onmouseout = removeHighlightSearchResult;
+	xmlHttp = getXMLHttpRequest();
 
-		  if (i === 0) {
-		  	settl_div.classList.add("search-result-selected");
+	xmlHttp.onreadystatechange=function() {
+		// Results found
+		if (this.readyState === 4 && this.status === 200) {
+
+		  suggestion_box.innerHTML = '';
+
+		  search_results = JSON.parse(this.responseText);
+
+		  // Adding search results as suggestions to suggestion box
+		  for (var i=0; i<search_results.length; i++) {
+
+			  var settlement_div = document.createElement('div');
+			  settlement_div.className = "search-result";
+
+			  settlement_div.onmouseover = addHighlightSearchResult;
+			  settlement_div.onmouseout = removeHighlightSearchResult;
+
+			  // Select first result by default
+			  if (i === 0) {
+				settlement_div.classList.add("search-result-selected");
+			  }
+
+			  var settlement_name = document.createElement('span');
+			  settlement_name.innerHTML = capitalizeFirstLetter(search_results[i].name);
+			  settlement_name.classList.add("settlement-name");
+
+			  var settl_district = document.createElement('span');
+			  settl_district.innerHTML = search_results[i].district + " район, ";
+
+			  var settl_region = document.createElement('span');
+			  settl_region.innerHTML = search_results[i].region + " область";
+
+
+			  var br = document.createElement("br");
+
+			  settlement_div.appendChild(settlement_name);
+			  settlement_div.appendChild(br);
+			  settlement_div.appendChild(settl_district);
+			  settlement_div.appendChild(settl_region);
+
+			  settlement_div.onclick = selectSettlement;
+			  settlement_div._ind = i;
+			  settlement_div._sttl_obj = search_results[i];
+
+			  suggestion_box.appendChild(settlement_div);
 		  }
-		  var settl_name = document.createElement('span');
-		  settl_name.innerHTML = capitalizeFirstLetter(search_results[i].name);
-		  settl_name.classList.add("settlement-name");
-		  
-		  var settl_district = document.createElement('span');
-		  settl_district.innerHTML = search_results[i].district + " район, ";
-		  //settl_district.classList.add("settlement-name");
-		  
-		  var settl_region = document.createElement('span');
-		  settl_region.innerHTML = search_results[i].region + " область";
-		  //settl_region.classList.add("settlement-name");
+		}
 
-		  var br = document.createElement("br");
-		  
-		  settl_div.appendChild(settl_name);	  
-		  settl_div.appendChild(br);		  
-		  settl_div.appendChild(settl_district);
-		  settl_div.appendChild(settl_region);
-		  
+		if (this.readyState === 4 && this.status === 404) {
 
-		  settl_div.onclick = selectSettlement;
-		  settl_div._ind = i;
-		  settl_div._sttl_obj = search_results[i];
-		  
-		  livesearch_div.appendChild(settl_div);
-	  }
-    }
-	
-	if (this.readyState==4 && this.status==404) {
-		livesearch_div.innerHTML = '';
-		 var settl_div_nr = document.createElement('div');
-		  
-		  settl_div_nr.className = "search-result";
-		  var settl_name_nr = document.createElement('span');
-		  settl_name_nr.innerHTML = "Не знайдено :(";
-		  settl_name_nr.classList.add("settlement-name");
-		  settl_div_nr.appendChild(settl_name_nr);
-		  
-		  livesearch_div.appendChild(settl_div_nr);
-	}
+			// Clear suggestion box
+			suggestion_box.innerHTML = '';
+
+			// Create new div for message
+			var settlement_div_no_result = document.createElement('div');
+			settlement_div_no_result.className = "search-result";
+
+			// Create "No result" message
+			var settl_name_nr = document.createElement('span');
+			settl_name_nr.innerHTML = "Не знайдено!";
+			settl_name_nr.classList.add("settlement-name");
+			settlement_div_no_result.appendChild(settl_name_nr);
+
+			// Add message to suggestion box
+			suggestion_box.appendChild(settlement_div_no_result);
+		}
 	
   };
-  
-  
-  
-  //setTimeout(function() {
-	  xmlhttp.open("GET","search/"+input_text,true);
-	  xmlhttp.send();	  
-	// }, 1000);
+
+	xmlHttp.open("GET", "search/" + input_text, true);
+	xmlHttp.send();
+
 }
 
 
@@ -218,116 +237,95 @@ function errorCheck(input) {
 
 function openSearchPage(event) {
 	event.preventDefault();
-	//var settl_id = document.getElementById("settlement-id");
 
-	if (errorCheck(input_from) || errorCheck(input_to)) {
+	var inputFromError = errorCheck(input_from);
+	var inputToError = errorCheck(input_to);
+
+	// Break if errors found
+	if (inputFromError || inputToError) {
 		return;
 	}
 
-	 var url = 'http://bus.com.ua/cgi-bin/poshuk?fp=UA'+ input_from._sttl_obj.koatuu + '&tp=UA' + input_to._sttl_obj.koatuu +'&Go=3';
-	 window.open(url,'_blank');
-	 return;
-	 
+	var url = 'http://bus.com.ua/cgi-bin/poshuk?fp=UA'+ input_from._sttl_obj.koatuu + '&tp=UA' + input_to._sttl_obj.koatuu +'&Go=3';
+	window.open(url,'_blank');
 }
 
-
 function handleKeyboardButtons(event) {
-	// Escape button
-	if (event.keyCode == 27) {
-		var livesearch_div_parent = this.parentNode;
+	// Find related suggestion box
+	var livesearch_div_parent = this.parentNode;
+	var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
+	var livesearch_div = result_of_livesearch[0];
 
-		var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
-		// console.log(result_of_livesearch);
-		var livesearch_div = result_of_livesearch[0];
+	// Find selected suggestion (where focus is)
+	var selected_result_array = livesearch_div.getElementsByClassName("search-result-selected");
+	var selected_result = selected_result_array[0];
 
+	// Hide suggestion box on ESCAPE
+	if (event.keyCode === ESCAPE_CODE) {
 		livesearch_div.style.display = 'none';
 	}
-	// Enter button
-	if (event.keyCode === 13) {
 
+	// Select suggestion on ENTER
+	if (event.keyCode === ENTER_CODE) {
+
+		// Prevent default action only when suggestion box is shown
 		if (this._sttl_obj === null || this._sttl_obj === undefined) {
 			event.preventDefault();
 		}
 
-		var livesearch_div_parent = this.parentNode;
-
-		var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
-		// console.log(result_of_livesearch);
-		var livesearch_div = result_of_livesearch[0];
-		var selected_result_array = livesearch_div.getElementsByClassName("search-result-selected");
-		var selected_result = selected_result_array[0];
-
-		if (selected_result != null) {
-			selectSettlementonEnter(selected_result);
+		if (selected_result !== null && selected_result !== undefined) {
+			selectSettlementBySuggestion(selected_result);
 		}
 
 	}
-	// Tab button
-	if (event.keyCode == 9) {
-		var livesearch_div_parent = this.parentNode;
+	// Select suggestion on TAB
+	if (event.keyCode === TAB_CODE) {
 
-		var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
-
-		var livesearch_div = result_of_livesearch[0];
-
-		//
-		if (livesearch_div.style.display != 'none') {
-			//event.preventDefault();
-
-			var selected_result_array = livesearch_div.getElementsByClassName("search-result-selected");
-			var selected_result = selected_result_array[0];
-
-			if (selected_result != null) {
-				selectSettlementonEnter(selected_result);
+		if (livesearch_div.style.display !== 'none') {
+			// Choose settlement if selected or hide suggestion box if not
+			if (selected_result !== null && selected_result !== undefined) {
+				selectSettlementBySuggestion(selected_result);
 			} else {
 				livesearch_div.style.display = 'none';
 			}
 		}
-
-
     }
-    // Up and Down arrows
-	if (event.keyCode == 38 || event.keyCode == 40) {
+    // Change selected suggestion on UP and DOWN ARROWS
+	if (event.keyCode === UP_ARROW_CODE || event.keyCode === DOWN_ARROW_CODE) {
 		event.preventDefault();
 
-		var livesearch_div_parent = this.parentNode;
-
-		var result_of_livesearch = livesearch_div_parent.getElementsByClassName("livesearch");
-		// console.log(result_of_livesearch);
-		var livesearch_div = result_of_livesearch[0];
 		var search_results_array = livesearch_div.getElementsByClassName("search-result");
-		//console.log(search_results_array.length);
 
+		// More than 1 suggestion can be selected
 		if (search_results_array.length > 1) {
-			var selected_result_array = livesearch_div.getElementsByClassName("search-result-selected");
-			var selected_result = selected_result_array[0];
-			//console.log(selected_result);
-			if (event.keyCode == 38) {
+
+			if (event.keyCode === UP_ARROW_CODE) {
 				var prev_result;
 
-				if (selected_result == null) {
+				// Check is there selected option presented
+				if (selected_result === null || selected_result === undefined) {
 					prev_result = livesearch_div.lastElementChild;
 				} else {
 					selected_result.classList.remove('search-result-selected');
 					prev_result = selected_result.previousElementSibling;
 				}
 
-				if (prev_result != null) {
-
+				if (prev_result !== null && prev_result !== undefined) {
 					prev_result.classList.add('search-result-selected');
 				}
 			}
-			if (event.keyCode == 40) {
+			if (event.keyCode === DOWN_ARROW_CODE) {
 				var next_result;
 
-				if (selected_result == null) {
+				// Check is there selected option presented
+				if (selected_result === null || selected_result === undefined) {
 					next_result = livesearch_div.firstElementChild;
 				} else {
 					next_result = selected_result.nextElementSibling;
 					selected_result.classList.remove('search-result-selected');
 				}
 
-				if (next_result != null) {
+				if (next_result !== null && next_result !== undefined) {
 
 					next_result.classList.add('search-result-selected');
 				}
@@ -339,9 +337,11 @@ function handleKeyboardButtons(event) {
 }
 
 // Hide search result list when click is made outside livesearch div
-function hideLiveSearch(event){
+function hideSuggestionBox(event){
+
 	var lives_to = document.getElementById('livesearch-to');
 	var lives_from = document.getElementById('livesearch-from');
+
 	if (event.target != lives_to) {
 		lives_to.style.display = 'none';
 	}
@@ -350,12 +350,10 @@ function hideLiveSearch(event){
 	}
 }
 
-
 /*
   Setting event handlers
 */
-
-document.onclick = hideLiveSearch;
+document.onclick = hideSuggestionBox;
 
 input_from.oninput = showSearchSuggestions;
 input_to.oninput = showSearchSuggestions;
